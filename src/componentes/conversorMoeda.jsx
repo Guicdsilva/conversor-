78a567ecd17fbe5c69aada1fbea094058846de45
Guilherme Carvalho <guilhermecdsilva0@gmail.com>
2025-10-13 19:11:58 -0300

    adicionando metro

--- src/componentes/conversorMoeda.jsx
+++ src/componentes/conversorMoeda.jsx
@@ -1,52 +1,122 @@
 
+
 import { useState ,useEffect} from "react";
 
  
-   function Moeda() {
+   function Conversor() {
+     
+      
+      const [valorEntrada,setValorEntrada] = useState('')
+      const [unidadeOrigem,setUnidadeOrigem] = useState("BRL")
+      const [unidadeDestino,setUnidadeDestino] = useState("USD")
+      const [resultado,setResultado] = useState("rgr") 
 
-     const taxasMoeda = {
+     const [unidade ,setUnidade]= useState('moeda')
+ 
+ const opcoesMoeda = [
+    { value: 'USD', label: 'Dólar (USD)' },
+    { value: 'BRL', label: 'Real (BRL)' },
+    { value: 'EUR', label: 'Euro (EUR)' },
+  ];
+
+ 
+  const opcoesMetro = [
+    { value: 'metros', label: 'Metros' },
+    { value: 'quilometros', label: 'Quilômetros' },
+    { value: 'centimetros', label: 'Centímetros' },
+  ];
+
+let opcoesAtuais;
+
+
+      if (unidade === 'moeda') {
+  opcoesAtuais = opcoesMoeda;
+  
+      }
+      else{
+  opcoesAtuais = opcoesMetro
+}
+
+      function inverter(){
+        
+            setUnidadeOrigem(unidadeDestino)
+            setUnidadeDestino(unidadeOrigem)
+        
+    
+ }
+
+if(isNaN(valorEntrada)){
+      alert(`preencha o campo (${valorEntrada})`)
+      setValorEntrada("")
+    }  
+
+   
+
+ function moeda(){
+  const taxasMoeda = {
        USD: 1,
        EUR: 0.85,
        BRL: 5.20,
        JPY: 110.0,
        AUD: 1.35
       };
-     
-      const [valorEntrada,setValorEntrada] = useState('')
-      const [moedaOrigem,setMoedaOrigem] = useState("USD")
-      const [moedaDeDestino,setMoedaDeDestino] = useState("BRL")
-      const [resultado ,setResultado] = useState("") 
-
- function conversor(){
- const moeda = valorEntrada / taxasMoeda[moedaOrigem]
-  const resul = moeda * taxasMoeda[moedaDeDestino]
+ const valor = valorEntrada / taxasMoeda[unidadeOrigem]
+  const resul = valor * taxasMoeda[unidadeDestino]
    // if(valorEntrada === ''){
      // alert(`preencha o campo (${valorEntrada})`)
      // return
   //  }
-    if(isNaN(valorEntrada)){
-      alert(`preencha o campo (${valorEntrada})`)
-      setValorEntrada("")
-    }
-    else{
-     setResultado(`${resul.toFixed(2)} ${moedaDeDestino}`) 
-    }
+    setResultado(resul.toFixed(2))
   }
- function inverter(){
-    setMoedaOrigem(moedaDeDestino)
-    setMoedaDeDestino(moedaOrigem)
- }
-  useEffect(() => {
-    conversor();
-  }, [valorEntrada, moedaOrigem ,moedaDeDestino]);
+
+    useEffect(() => {
+      if(unidade === 'metro'){
+        comprimento();
+      }else{
+        moeda()
+      }
+    },[valorEntrada, unidadeOrigem ,unidadeDestino]);
+
+    function comprimento(){
+      const fatoresComprimento = {
+      metros: 1,
+     quilometros: 1000,
+     centimetros: 0.01
+};   
+   const valor = valorEntrada * fatoresComprimento[unidadeOrigem]
+   const resul = valor / fatoresComprimento[unidadeDestino]
+
+   setResultado(resul.toFixed(2))
+    }
+  
+    
+   function escolherUnidade(e){
+    const novaUnidade = e.target.value
+     setUnidade(novaUnidade)
+     if (novaUnidade === 'moeda') {
+    setUnidadeOrigem('USD');
+    setUnidadeDestino('BRL');
+  } else {
+    setUnidadeOrigem('metros');
+    setUnidadeDestino('quilometros');
+  }
+     
+   }
 
  return (
 <div>
-<section className="conversor" data-tipo="moeda">
-      <h2>Moeda</h2>
+<section className="conversor" >
+
+  <label >escolha Unidade:</label>
+  <select 
+  onChange={escolherUnidade}
+  value={unidade}
+  >
+    <option value='metro' >metro</option>
+    <option value='moeda'>moeda</option>
+  </select>
 
 
-      
       <label>Quero converter:</label>
       <input  className="valor-input" 
       placeholder=" digite o valor: "
@@ -54,48 +124,43 @@ import { useState ,useEffect} from "react";
       onChange={(e) => setValorEntrada(e.target.value)}
       />
 
-
-
-        <label>De:</label>
-  
-      <select className="de-unidade"
+               
+        <label >De:</label>
       
-      value={moedaOrigem}
-      onChange={(e) => setMoedaOrigem(e.target.value)}
+      <select className="de-unidade"
+      value={unidadeOrigem}
+      onChange={(e) => setUnidadeOrigem(e.target.value)}
       >
-
-        <option value="USD">Dólar (USD)</option>
-        <option value="EUR" >Euro (EUR)</option>
-        <option value="BRL">Real (BRL)</option>
+       {opcoesAtuais.map((opcao) =>(
+        <option key={opcao.value} value={opcao.value}>
+          {opcao.label}
+          
+        </option>
+   ))}
       </select>
       
-       
-
-
+    
   <input type="button" className="converter-btn" onClick={inverter} value="inverter"/>
 
+
+
       <label>Para:</label>
+     
       <select className="para-unidade"
-      value={moedaDeDestino}
-      onChange={(e) => setMoedaDeDestino(e.target.value)}>
-        <option value="BRL">Real (BRL)</option>
-        <option value="USD">Dólar (USD)</option>
-        <option value="EUR">Euro (EUR)</option>
-        <option value="JPY">Iene (JPY)</option>
-        <option value="AUD">Dólar Australiano (AUD)</option>
+      value={unidadeDestino}
+      onChange={(e) => setUnidadeDestino(e.target.value)}>
+        {opcoesAtuais.map((opcao) =>(
+        <option key={opcao.value} value={opcao.value}>
+          {opcao.label}
+          
+        </option>
+   ))}
       </select>
-
-
-
-
-
-      
-
+ 
 
       <div className="resultado">
         <input  className="resultado-input" readOnly
         value={resultado}
-         
         />
         
       </div>  
@@ -105,5 +170,5 @@ import { useState ,useEffect} from "react";
  )
  }
  
-export default Moeda
+export default Conversor
  
